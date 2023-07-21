1889232aa5aedd8109bfbbdce53eab0ca948167c
R.J. Lorimer <rj@realjenius.com>
2023-07-21 17:48:43 -0500

    Shipit

--- functions/.well-known/webfinger.js
+++ functions/.well-known/webfinger.js
@@ -6,5 +6,10 @@ export function onRequestGet(context) {
         "rel": "http://openid.net/specs/connect/1.0/issuer",
         "href": "https://auth.realjenius.com"
       }]
-    }`);
+    }`, {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
 }
